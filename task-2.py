bfbf52484d2b47082df7bc9c52c1d25ce5debbe2
Alex Khvostov <alexandertail89@gmail.com>
2019-06-04 02:40:12 +0700

    Modified task-2.py and task-3.py

--- task-2.py
+++ task-2.py
@@ -1,6 +1,6 @@
 """
 Имеется положительное число. Необходимо найти сумму его цифр. Если
-передается не число, вернуть 'Это не число!'
+передается не число, вывести 'Это не число!'
 """
 
 # Если some_number == 45
@@ -15,19 +15,63 @@
 # Если some_number == {'key' : 'value'}
 # то 'Это не число!'
 
-some_number = 1231421
-
-def sum_of_numbers(number):
-        len_of_numbers = len(str(number))
-        result = 0
-        for n in range(len_of_numbers):
-            result += int(str(number)[n])
-        return result
-
-print('Сумма =', sum_of_numbers(some_number))
 
 
+# Решение № 1 ----------------------------------------------------------
+num = input("1) Введите число: ")
 
-        
-    
+if num.isnumeric():
+        print('1) Cумма чисел: ', sum(map(int, num)))
+else:
+        print("1) Это не число!")
+print('\n')
 
+
+
+# Решение № 2 ----------------------------------------------------------
+
+some_number = 197
+
+def sum_of_digits(number: int):
+        if str(number).isnumeric():
+                print(type(number))
+                digit_list = map(int, str(number))
+                print('2) Cумма чисел: ', sum(digit_list))
+        else:
+                print("2) Это не число!")
+                
+sum_of_digits(some_number)
+print('\n')
+
+
+
+# Решение № 3 ----------------------------------------------------------
+
+some_num = 197
+
+def sum_of_digits(number: int):
+    if str(number).isnumeric():
+        print('3) Cумма чисел: ', sum([int(i) for i in str(number)]))
+    else:
+        print("3) Это не число!")
+sum_of_digits(some_num)
+print('\n')
+
+
+
+# Решение № 4 ----------------------------------------------------------
+
+some_number = input("4) Введите число: ")
+
+def sum_of_numbers(number: str):
+        if number.isnumeric():
+                len_of_numbers = len(number)
+                result = 0
+                for n in range(len_of_numbers):
+                    result += int(number[n])
+                print('4) Сумма чисел =', result)
+        else:
+                print("4) Это не число!")
+
+
+sum_of_numbers(some_number)
--- task-3.py
+++ task-3.py
@@ -11,21 +11,25 @@
 #   return 99999
 
 # if some_number == 1
-#   return 1
+#   return 9
 
 # if some_number == 0
 #   return 0
 
 
-# Решение №1
+
+# Решение №1 -----------------------------------------------------------
+
 print(int("9"*int(input('1) Введите целое число до 1000: '))))
 
 
-# Решение №2
+
+# Решение №2 -----------------------------------------------------------
+
 def max_number(some_number: int) -> int:
     return 10 ** some_number - 1
 
 print(max_number(int(input('2) Введите целое число до 1000: '))))
-        
+
 
 
